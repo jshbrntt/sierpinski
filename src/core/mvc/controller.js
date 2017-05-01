5d229922967cbf42206781bf0927288ff3ec5891
Joshua Barnett <jbacontrol@gmail.com>
2017-05-01 23:17:41 +0100

    Add basic MVC classes.

---
+++ src/core/mvc/controller.js
@@ -0,0 +1,12 @@
+export default class Controller {
+  constructor (model, view) {
+    this._model = model
+    this._view = view
+  }
+  get view () {
+    return this._view
+  }
+  get model () {
+    return this._model
+  }
+}
