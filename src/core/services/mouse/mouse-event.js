c03402fe877c3bce494bb26035b8daad1e42100c
Joshua Barnett <jbacontrol@gmail.com>
2017-05-01 23:20:45 +0100

    Add mouse service, and event enum.

---
+++ src/core/services/mouse/mouse-event.js
@@ -0,0 +1,8 @@
+let MouseEvent = {
+  DOWN: 'core.MouseEvent.DOWN',
+  UP: 'core.MouseEvent.UP',
+  CLICK: 'core.MouseEvent.CLICK',
+  MOVE: 'core.MouseEvent.MOVE',
+  WHEEL: 'core.MouseEvent.WHEEL'
+}
+export default MouseEvent
