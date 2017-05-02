bb255f69f05282e8b38a5602ddeb0aa82fed8f84
Joshua Barnett <jbacontrol@gmail.com>
2017-05-02 20:35:42 +0100

    Refactored equilateral triangle calculation and geometry creation into reusable pure static methods.

--- src/sierpinski/models/triangle-model.js
+++ src/sierpinski/models/triangle-model.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import { Geometry, Face3, Vector3, Triangle } from 'three'
 import Model from './../../core/mvc/model'
 
 export default class TriangleModel extends Model {
@@ -6,22 +6,30 @@ export default class TriangleModel extends Model {
     super()
 
     this._width = width
-    this._height = width * Math.sin(Math.PI / 3)
+    this._height = TriangleModel.calculateHeight(this._width)
 
-    this._geometry = new THREE.Geometry()
+    this._geometry = TriangleModel.createGeometry(this._width, this._height)
+  }
+  static calculateHeight (width) {
+    return width * Math.sin(Math.PI / 3)
+  }
+  static createGeometry (width, height) {
+    let geometry = new Geometry()
 
-    let v1 = new THREE.Vector3(this._width / -2, this._height / -2, 0)
-    let v2 = new THREE.Vector3(this._width / 2, this._height / -2, 0)
-    let v3 = new THREE.Vector3(0, this._height + this._height / -2, 0)
+    let v1 = new Vector3(width / -2, height / -2, 0)
+    let v2 = new Vector3(width / 2, height / -2, 0)
+    let v3 = new Vector3(0, height + height / -2, 0)
 
-    this._triangle = new THREE.Triangle(v1, v2, v3)
-    this._normal = this._triangle.normal()
+    let triangle = new Triangle(v1, v2, v3)
+    let normal = triangle.normal()
 
-    this._geometry.vertices.push(this._triangle.a)
-    this._geometry.vertices.push(this._triangle.b)
-    this._geometry.vertices.push(this._triangle.c)
+    geometry.vertices.push(triangle.a)
+    geometry.vertices.push(triangle.b)
+    geometry.vertices.push(triangle.c)
 
-    this._geometry.faces.push(new THREE.Face3(0, 1, 2, this._normal))
+    geometry.faces.push(new Face3(0, 1, 2, normal))
+
+    return geometry
   }
   get width () {
     return this._width
